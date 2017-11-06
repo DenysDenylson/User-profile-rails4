77c9d906a19ac34cb6bfd234bc4b7038b9e86f55
DenysDenylson <DenysDenylson@gmail.com>
2017-11-06 17:55:35 -0400

    PASO 1.1 : Agregar nuevos valores a User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106213551) do
+ActiveRecord::Schema.define(version: 20171106214603) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20171106213551) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "role"
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
